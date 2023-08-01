bf1410474d7d7443ee746aed0c29752d73ef3b98
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-01 14:15:25 +0100

    implemented initial set up of getStaticPaths and getStaticProps for our dynamically displayed blog page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,3 +3,11 @@ import Layout from "../../components/layout";
 export default function Post() {
   return <Layout></Layout>;
 }
+
+export async function getStaticPaths() {
+  //return a list of possible value for id
+}
+
+export async function getStaticProps({ params }) {
+  //fetch necessary data for the blog post using params.id
+}
